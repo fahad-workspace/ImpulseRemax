e1a8d3883eba4c1bcfc3283004569f45eda2bee0
Fahad Sarwar <fahad.workspace@outlook.com>
2015-05-27 16:11:39 +0530

    Added database.yml

--- db/schema.rb
+++ db/schema.rb
@@ -14,36 +14,36 @@
 ActiveRecord::Schema.define(version: 20150524095349) do
 
   create_table "cities", force: :cascade do |t|
-    t.string "city_name"
+    t.string   "city_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "impulse_databases", force: :cascade do |t|
     t.integer "property_id"
-    t.string "property_type"
-    t.float "budget"
+    t.string  "property_type"
+    t.float   "budget"
     t.integer "number_of_rooms"
-    t.string "city"
-    t.string "locality"
+    t.string  "city"
+    t.string  "locality"
     t.integer "contact_number"
-    t.date "posted_on"
+    t.date    "posted_on"
   end
 
   create_table "promos", force: :cascade do |t|
-    t.string "promo_heading"
-    t.string "promo_content"
-    t.string "promo_extra_content"
-    t.string "promo_number"
-    t.integer "promo_priority"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "promo_heading"
+    t.string   "promo_content"
+    t.string   "promo_extra_content"
+    t.string   "promo_number"
+    t.integer  "promo_priority"
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
   end
 
   create_table "types", force: :cascade do |t|
-    t.string "property_type"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "property_type"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
 end
